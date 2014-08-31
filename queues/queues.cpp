849da7711ffeb65f6d3fbe5413754036bc6678f9
Brett Gottula <bgottula@gmail.com>
2014-08-30 21:54:48 -0700

    Added support for blocks with variable numbers of inputs and outputs

--- queues/queues.cpp
+++ queues/queues.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include <queue>
 #include <stdexcept>
+#include <cassert>
 
 using namespace std;
 
@@ -9,41 +10,87 @@ using namespace std;
 class Block
 {
     public:
+        Block(int numInputs = 1, int numOutputs = 1)
+            : m_numInputs(numInputs), m_numOutputs(numOutputs)
+        {
+            cout << "Creating block with " << numInputs << " inputs and "
+                << numOutputs << " outputs." << endl;
+            if (numInputs == 0 && numOutputs == 0)
+            {
+                throw range_error("Block must have at least one port");
+            }
+            if (numInputs < 0 || numOutputs < 0)
+            {
+                throw range_error("Block can't have negative number of ports");
+            }
+            m_input = new queue<int>[m_numInputs];
+            m_output = new queue<int>[m_numOutputs];
+            reset();
+        }
+
         void reset(void)
         {
-            m_input = queue<int>();
-            m_output = queue<int>();
+            for (int i = 0; i < m_numInputs; i++)
+            {
+                m_input[i] = queue<int>();
+            }
+            for (int i = 0; i < m_numOutputs; i++)
+            {
+                m_output[i] = queue<int>();
+            }
         }
         
         virtual void work(void) = 0;
 
-        void pushInput(int input)
+        void pushInput(int input, int inputIndex = 0)
         {
-            m_input.push(input);
+            assert(inputIndex >= 0 && inputIndex < m_numInputs);
+            m_input[inputIndex].push(input);
         }
 
-        bool outputEmpty(void)
+        bool outputEmpty(int outputIndex = 0)
         {
-            return m_output.empty();
+            assert(outputIndex >= 0 && outputIndex < m_numOutputs);
+            return m_output[outputIndex].empty();
         }
 
-        int popOutput(void)
+        int popOutput(int outputIndex = 0)
         {
-            if (m_output.empty())
+            assert(outputIndex >= 0 && outputIndex < m_numOutputs);
+            if (m_output[outputIndex].empty())
             {
                 throw runtime_error("Tried to pop from empty output queue!");
                 return 0;
             }
 
-            int output = m_output.front();
-            m_output.pop();
+            int output = m_output[outputIndex].front();
+            m_output[outputIndex].pop();
             return output;
         }
 
     protected:
-        queue<int> m_input;
-        queue<int> m_output;
+        queue<int> *m_input;
+        const int m_numInputs;
+        queue<int> *m_output;
+        const int m_numOutputs;
 };
+
+class SourceBlock : public Block
+{
+    public:
+        SourceBlock(int numOutputs = 1)
+            : Block(0, numOutputs)
+        {}
+};
+
+class SinkBlock : public Block
+{
+    public:
+        SinkBlock(int numInputs = 1)
+            : Block(numInputs, 0)
+        {}
+};
+
 
 class Scheduler
 {
@@ -79,14 +126,9 @@ class Scheduler
         list<Block *> m_blocks;
 };
 
-class Source : public Block
+class Source : public SourceBlock
 {
     public:
-        Source()
-        {
-            reset();
-        }
-
         void reset(void)
         {
             Block::reset();
@@ -95,7 +137,7 @@ class Source : public Block
 
         void work(void)
         {
-            m_output.push(m_counter++); 
+            m_output[0].push(m_counter++); 
         }
 
     private:
@@ -105,11 +147,6 @@ class Source : public Block
 class Decimator : public Block
 {
     public:
-        Decimator()
-        {
-            reset();
-        }
-
         void reset(void)
         {
             Block::reset();
@@ -118,13 +155,13 @@ class Decimator : public Block
 
         void work(void)
         {
-            while (!m_input.empty())
+            while (!m_input[0].empty())
             {
                 if (m_counter % 2 == 0)
                 {
-                    m_output.push(m_input.front());
+                    m_output[0].push(m_input[0].front());
                 }
-                m_input.pop();
+                m_input[0].pop();
                 m_counter++;
             }
         }
@@ -136,11 +173,6 @@ class Decimator : public Block
 class Interpolator : public Block
 {
     public:
-        Interpolator()
-        {
-            reset();
-        }
-
         void reset(void)
         {
             Block::reset();
@@ -148,29 +180,24 @@ class Interpolator : public Block
 
         void work(void)
         {
-            while (!m_input.empty())
+            while (!m_input[0].empty())
             {
-                m_output.push(m_input.front());
-                m_output.push(m_input.front());
-                m_input.pop();
+                m_output[0].push(m_input[0].front());
+                m_output[0].push(m_input[0].front());
+                m_input[0].pop();
             }
         }
 };
 
-class Sink : public Block
+class Sink : public SinkBlock
 {
     public:
-        Sink()
-        {
-            reset();
-        }
-
         void work(void)
         {
-            while (!m_input.empty())
+            while (!m_input[0].empty())
             {
-                cout << "Sink got " << m_input.front() << endl;
-                m_input.pop();
+                cout << "Sink got " << m_input[0].front() << endl;
+                m_input[0].pop();
             }
         }
 };
