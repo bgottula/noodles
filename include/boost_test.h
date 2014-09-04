28b1bdb3a5c105eab8c25806aa9286cf9c9fd9a8
Justin Gottula <justin@jgottula.com>
2014-09-04 14:00:25 -0700

    remove warnings from boost UTF common header

--- include/boost_test.h
+++ include/boost_test.h
@@ -4,14 +4,11 @@
 #define BOOST_TEST_MAIN
 
 #if USE_BOOST_UTF_DYNAMIC
-	#warning using dynamic
 	#define BOOST_TEST_DYN_LINK
 	#include <boost/test/unit_test.hpp>
 #elif USE_BOOST_UTF_STATIC
-	#warning using static
 	#include <boost/test/unit_test.hpp>
 #elif USE_BOOST_UTF_HEADER
-	#warning using header
 	#include <boost/test/included/unit_test.hpp>
 #else
 	#error must define one of: USE_BOOST_UTF_{DYNAMIC,STATIC,HEADER}
