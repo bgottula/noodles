c8b60cd3c77edd40589287610b33f15c72f1407a
Justin Gottula <justin@jgottula.com>
2014-09-16 12:24:33 -0700

    s/NoodlesTest/GraphTest/g

---
+++ src/unit/GraphTest.cpp
@@ -0,0 +1,32 @@
+#include "std.h"
+#include "boost_test.h"
+#include "Graph.h"
+#include "Block.h"
+
+class MockBlock : public Block
+{
+	void work(void) {}
+};
+
+BOOST_AUTO_TEST_CASE( graphTest )
+{
+	MockBlock block1;
+	MockBlock block2;
+	MockBlock block3;
+	Graph dut;
+
+	/* No connections yet */
+	BOOST_CHECK(dut.checkGraph() >= 1);
+
+	/* Add one valid noodle */
+	Noodle n1(&block1, &block2);
+	dut.addNoodle(&n1);
+	BOOST_CHECK_EQUAL(dut.checkGraph(), 0);
+
+	/* Try to add the same noodle again */
+	BOOST_CHECK_THROW(dut.addNoodle(&n1), std::runtime_error);
+
+	/* Try to make block1 and block3 be the source for block2 */
+	Noodle n2(&block3, &block2);
+	BOOST_CHECK_THROW(dut.addNoodle(&n2), std::runtime_error);
+}
