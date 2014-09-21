62425672de78026e1af4912f2cb0a46b3b5e7f5b
Brett Gottula <bgottula@gmail.com>
2014-09-20 20:50:06 -0700

    GraphTest: Added test case for multiple noodles from a single block output port

--- src/unit/GraphTest.cpp
+++ src/unit/GraphTest.cpp
@@ -6,11 +6,12 @@ class MockBlock : public Block
 	void work(void) {}
 };
 
-BOOST_AUTO_TEST_CASE( graphTest )
+BOOST_AUTO_TEST_CASE( graphValidityTests )
 {
 	MockBlock block1;
 	MockBlock block2;
 	MockBlock block3;
+	MockBlock block4;
 	Graph dut;
 
 	/* No connections yet */
@@ -24,7 +25,14 @@ BOOST_AUTO_TEST_CASE( graphTest )
 	/* Try to add the same noodle again */
 	BOOST_CHECK_THROW(dut.addNoodle(&n1), DuplicateNoodleException);
 
-	/* Try to make block1 and block3 be the source for block2 */
+	/* Try to make block1 and block3 be the source for block2 (not ok) */
 	Noodle n2(&block3, &block2);
 	BOOST_CHECK_THROW(dut.addNoodle(&n2), InputMultipleNoodleException);
+
+	/* Make block2 be the source for block3 and block4 (allowed) */
+	Noodle n3(&block2, &block3);
+	Noodle n4(&block2, &block4);
+	dut.addNoodle(&n3);
+	dut.addNoodle(&n4);
+	BOOST_CHECK_EQUAL(dut.checkGraph(), 0);
 }
