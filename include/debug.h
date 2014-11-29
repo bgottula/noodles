7998e44110b346e4fa8eb36500ea117f483ab02e
Justin Gottula <justin@jgottula.com>
2014-11-29 13:46:10 -0800

    invent StrPool class for temporarily storing strings

--- include/debug.h
+++ include/debug.h
@@ -5,4 +5,15 @@ extern bool verbose;
 
 void debug(const char *format, ...) __attribute__((format(printf, 1, 2)));
 
+class StrPool
+{
+public:
+	char *alloc(size_t len);
+	void clear(void);
+	
+private:
+	mutex m_mutex;
+	list<const char *> m_strs;
+};
+
 #endif
--- src/debug.cpp
+++ src/debug.cpp
@@ -12,3 +12,24 @@ void debug(const char *format, ...)
 		va_end(va);
 	}
 }
+
+char *StrPool::alloc(size_t len)
+{
+	char *str = new char[len];
+	
+	lock_guard<mutex> lock(m_mutex);
+	m_strs.push_back(str);
+	
+	return str;
+}
+
+void StrPool::clear(void)
+{
+	lock_guard<mutex> lock(m_mutex);
+	
+	while (!m_strs.empty())
+	{
+		delete[] m_strs.front();
+		m_strs.pop_front();
+	}
+}
