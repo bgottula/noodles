a7050d2f1382e0399894ed3430463edf06812254
Justin Gottula <justin@jgottula.com>
2014-11-29 07:35:30 -0800

    Noodle: create QNoodle and RNoodle subclasses

--- include/Noodle.h
+++ include/Noodle.h
@@ -1,13 +1,41 @@
 #ifndef NOODLE_H
 #define NOODLE_H
 
+/* generic noodle class */
 class Noodle
 {
 	friend class Graph;
 	
 public:
-	Noodle(size_t max, Endpoint from, Endpoint to)
-		: m_max(max), m_from(from), m_to(to) {};
+	Noodle(Endpoint from, Endpoint to)
+		: m_from(from), m_to(to) {};
+	
+	virtual size_t max(void) = 0;
+	virtual size_t count(void) = 0;
+	virtual size_t free(void) = 0;
+	
+	virtual void push(int sample) = 0;
+	virtual int pop(void) = 0;
+	virtual int peek(size_t where) = 0;
+	
+protected:
+	/* currently these endpoint structs are only actually used in
+	 * Graph::dumpGraph so we can easily iterate over all of the noodles and
+	 * see which blocks they are connected to in order to print a nice diagram
+	 * for the user; in no other case do we need to quickly iterate over the
+	 * list of noodles in this way; they can be ripped out later if desired */
+	Endpoint m_from;
+	Endpoint m_to;
+};
+
+/* queue noodle: buffers a certain number of samples */
+class QNoodle : public Noodle
+{
+	friend class Graph;
+	
+public:
+	QNoodle(size_t max, Endpoint from, Endpoint to)
+		: Noodle(from, to), m_max(max) {};
 	
 	/* get queue's max allowed sample count */
 	size_t max(void) { return m_max; }
@@ -26,6 +54,7 @@ public:
 	/* NEEDS LOCK: peek one sample from a given position in the queue */
 	int peek(size_t where);
 	
+#warning TODO
 	// TODO: find out if peek really needs the mutex to be held
 	
 private:
@@ -36,14 +65,34 @@ private:
 	
 	/* maximum number of samples that the queue is allowed to contain */
 	const size_t m_max;
+};
+
+/* register noodle: holds only one sample, has init value, never blocks */
+class RNoodle : public Noodle
+{
+	friend class Graph;
 	
-	/* currently these endpoint structs are only actually used in
-	 * Graph::dumpGraph so we can easily iterate over all of the noodles and
-	 * see which blocks they are connected to in order to print a nice diagram
-	 * for the user; in no other case do we need to quickly iterate over the
-	 * list of noodles in this way; they can be ripped out later if desired */
-	Endpoint m_from;
-	Endpoint m_to;
+public:
+	RNoodle(int init, Endpoint from, Endpoint to)
+		: Noodle(from, to), m_reg(init) {};
+	
+	/* register can always hold just one sample */
+	size_t max(void) { return 1; }
+	/* register can always furnish one get */
+	size_t count(void) { return 1; }
+	/* register can always furnish one put */
+	size_t free(void) { return 1; }
+	
+	/* write to the sample register */
+	void push(int sample) { m_reg = sample; }
+	/* read from the sample register */
+	int pop(void) { return m_reg; }
+	/* peek the sample register (same as pop) */
+	int peek(size_t where) { return m_reg; }
+	
+private:
+	/* register containing the current sample value */
+	int m_reg;
 };
 
 #endif
--- src/Noodle.cpp
+++ src/Noodle.cpp
@@ -1,19 +1,19 @@
 #include "all.h"
 
-void Noodle::push(int sample)
+void QNoodle::push(int sample)
 {
 	assert(m_queue.size() <= m_max);
 	m_queue.push_back(sample);
 }
 
-int Noodle::pop(void)
+int QNoodle::pop(void)
 {
 	int sample = m_queue.front();
 	m_queue.pop_front();
 	return sample;
 }
 
-int Noodle::peek(size_t where)
+int QNoodle::peek(size_t where)
 {
 #warning TODO
 	// TODO
