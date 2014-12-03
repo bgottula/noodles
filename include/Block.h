b10e56eaefeaf36f64e320916b38e6874e91b91c
Justin Gottula <justin@jgottula.com>
2014-12-02 22:24:40 -0800

    update class Block for Port changes

--- include/Block.h
+++ include/Block.h
@@ -1,6 +1,8 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#define REGISTER_PORT(_p) register_port(#_p, &_p)
+
 class Block
 {
 	friend class Graph;
@@ -10,14 +12,34 @@ public:
 	
 	virtual void reset(void) = 0;
 	virtual void work(void) = 0;
+	
 	const char *name(void) const;
 	
 protected:
-	InputPorts inputs;
-	OutputPorts outputs;
+	void register_port(const char *name, Port *p);
+	void list_ports(void);
 	
 private:
 	mutable char *m_name = nullptr;
+	
+	vector<NamedPort> m_ports;
+};
+
+class DuplicatePortException : public runtime_error
+{
+public: DuplicatePortException(void) :
+	runtime_error("Blocks cannot contain duplicate ports") {};
+};
+class DuplicatePortNameException : public runtime_error
+{
+public: DuplicatePortNameException(void) :
+	runtime_error("Blocks must contain ports with unique names") {};
+};
+
+class NonexistentPortException : public runtime_error
+{
+public: NonexistentPortException(void) :
+	runtime_error("Port with that name has not been registered") {};
 };
 
 #endif
--- src/Block.cpp
+++ src/Block.cpp
@@ -8,6 +8,72 @@ Block::~Block(void)
 	}
 }
 
+void Block::register_port(const char *name, Port *p)
+{
+	int status;
+	char *demangled = abi::__cxa_demangle(typeid(*p).name(),
+		nullptr, nullptr, &status);
+	assert(status == 0);
+	
+	printf("register_port: name %s type %s addr %p avail %zu\n",
+		name, demangled, p, p->available());
+	
+	if (any_of(m_ports.cbegin(), m_ports.cend(),
+		[&](const NamedPort& np) {
+			return (np.port == p);
+		}))
+	{
+		throw DuplicatePortException();
+	}
+	if (any_of(m_ports.cbegin(), m_ports.cend(),
+		[&](const NamedPort& np) {
+			return (strcmp(name, np.name) == 0);
+		}))
+	{
+		throw DuplicatePortNameException();
+	}
+	
+	NamedPort np { .name = name, .port = p };
+	m_ports.push_back(np);
+}
+
+void Block::list_ports(void)
+{
+	for (auto it = m_ports.begin(); it != m_ports.end(); ++it)
+	{
+		NamedPort& np = *it;
+		Port *p = np.port;
+		
+		decltype(*p) x = *p;
+		{
+			int status;
+			char *demangled = abi::__cxa_demangle(typeid(x).name(),
+				nullptr, nullptr, &status);
+			assert(status == 0);
+			
+			printf("list_ports: name %s type %s addr %p avail %zu\n",
+				np.name, demangled, &x, x.available());
+		}
+		
+		IPort *i = dynamic_cast<IPort *>(p);
+		OPort *o = dynamic_cast<OPort *>(p);
+		
+		if (i != nullptr) printf("is an input port.\n");
+		if (o != nullptr) printf("is an output port.\n");
+		
+		// TODO: how do we do this kind of thing, but generically??
+		/*InputPort<int> *ip_int = dynamic_cast<InputPort<int> *>(p);
+		if (ip_int != nullptr) printf("ip_int = %p\n", ip_int);
+		InputPort<float> *ip_float = dynamic_cast<InputPort<float> *>(p);
+		if (ip_float != nullptr) printf("ip_float = %p\n", ip_float);
+		OutputPort<int> *op_int = dynamic_cast<OutputPort<int> *>(p);
+		if (op_int != nullptr) printf("op_int = %p\n", op_int);
+		OutputPort<float> *op_float = dynamic_cast<OutputPort<float> *>(p);
+		if (op_float != nullptr) printf("op_float = %p\n", op_float);
+		*/
+	}
+}
+
 const char *Block::name(void) const
 {
 	/* only do this the first time the function is called for this instance */
