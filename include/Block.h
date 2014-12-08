c558dd66be5038896244ff6318ebb7eed7e451f2
Justin Gottula <justin@jgottula.com>
2014-12-07 17:53:41 -0800

    use IKnowsSubclassName and IContainsPorts in class Block

--- include/Block.h
+++ include/Block.h
@@ -1,10 +1,10 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#define REGISTER_PORT(_p) register_port(#_p, &_p)
-
 /* base for block classes (protected ctor prevents direct instantiation) */
-class Block
+class Block :
+	public IKnowsSubclassName,
+	public IContainsPorts
 {
 public:
 	virtual ~Block() {}
@@ -16,44 +16,15 @@ public:
 	
 	void check(void) const;
 	
-	Port *find_port(const char *p_name);
-	
-	const char *name(void) const;
-	
 protected:
 	Block() {}
-	
-	void register_port(const char *p_name, Port *p);
-	void list_ports(void);
-	
-private:
-	mutable const char *m_name = nullptr;
-	
-	vector<NamedPort> m_ports;
 };
 
-/* associates a name with a port */
+/* associates a name with a block */
 struct NamedBlock
 {
 	const char *name;
 	Block *block;
-};
-
-class BlockDuplicatePortException : public runtime_error
-{
-public: BlockDuplicatePortException(void) :
-	runtime_error("Port instances may only be registered once") {};
-};
-class BlockDuplicatePortNameException : public runtime_error
-{
-public: BlockDuplicatePortNameException(void) :
-	runtime_error("Port instances must be registered with unique names") {};
-};
-
-class BlockNonexistentPortException : public runtime_error
-{
-public: BlockNonexistentPortException(void) :
-	runtime_error("Port with that name has not been registered") {};
 };
 
 #endif
