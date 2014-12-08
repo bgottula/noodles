dc1d8454340fc90891d71a436eababb3a11d7998
Justin Gottula <justin@jgottula.com>
2014-12-07 16:08:48 -0800

    fix RoundRobinScheduler

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -15,28 +15,29 @@ void RoundRobinScheduler::run(void)
 
 void RoundRobinScheduler::run_once(void)
 {
-#warning FINISHME
-#if 0
+	debug(AT_BLD "RoundRobinScheduler::run_once" AT_RST "\n");
+	
 	/* call work on all blocks */
 	auto m_blocks = m_graph->m_blocks;
-	for (auto it = m_blocks->begin(); it != m_blocks->end(); ++it)
+	for (auto it = m_blocks.begin(); it != m_blocks.end(); ++it)
 	{
-		Block *b = *it;
+		NamedBlock nb = *it;
+		Block *b = nb.block;
 		
 		bool can_work = b->can_work();
-		debug("RoundRobinScheduler: block %s(%p) can%s do work\n",
-			(can_work ? "" : "not"), b->name(), b);
+		debug(AT_BLD "RoundRobinScheduler::run_once:" AT_RST
+			" block %s(%p) can%s do work\n",
+			b->name(), b, (can_work ? "" : "not"));
 		
 		if (can_work)
 		{
-			debug("RoundRobinScheduler: calling work on block %s(%p)\n",
-				b->name(), b);
+			debug(AT_BLD "RoundRobinScheduler::run_once:" AT_RST
+			" calling work on block %s(%p)\n", b->name(), b);
 			b->work();
 		}
 		
 		m_graph->dump(false, true);
 	}
-#endif
 }
 
 void ThreadedScheduler::run(void)
